a802550160d5a9cd1c3e1e95d3e99ac646e89134
gtc0823 <118497856+gtc0823@users.noreply.github.com>
2024-01-01 01:40:32 +0800

    Update GoogleQuery.java
    merge出了些問題，只能這樣更新了

--- src/GoogleQuery.java
+++ src/GoogleQuery.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -20,106 +21,102 @@ public class GoogleQuery
 	
 	public GoogleQuery(String searchKeyword)
 	{
-		
 		this.searchKeyword = searchKeyword;
-		try {
-			searchKeyword=java.net.URLEncoder.encode(searchKeyword,"utf-8");
-			this.url = "http://www.google.com/search?q="+searchKeyword+"電影"+"&oe=utf8&num=20";
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-		
+		this.url = "http://www.google.com/search?q="+searchKeyword+"電影"+"&oe=utf8&num=10";
 	}
 	
-	private String fetchContent() throws IOException
-	{
-		String retVal = "";
+	private String fetchContent() throws IOException {
+	    String retVal = "";
 
-		URL u = new URL(url);
-		URLConnection conn = u.openConnection();
-		//set HTTP header
-		conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
-		InputStream in = conn.getInputStream();
+	    URL u = new URL(url);
+	    URLConnection conn = u.openConnection();
+	    // set HTTP header
+	 // 在 fetchContent 方法中的 URLConnection 前加上以下代碼
+	    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+	    conn.setRequestProperty("Referer", "http://www.google.com/");
+	    conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
-		InputStreamReader inReader = new InputStreamReader(in, "utf-8");
-		BufferedReader bufReader = new BufferedReader(inReader);
-		String line = null;
 
-		while((line = bufReader.readLine()) != null)
-		{
-			retVal += line;
-		}
-		return retVal;
+	    InputStream in = conn.getInputStream();
+
+	    try (InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+	         BufferedReader bufReader = new BufferedReader(inReader)) {
+
+	        String line = null;
+
+	        while ((line = bufReader.readLine()) != null) {
+	            retVal += line;
+	        }
+	    }
+
+	    return retVal;
 	}
+
 	
-	public HashMap<String, String> query() throws IOException
-	{
-		if(content == null)
-		{
-			content = fetchContent();
-		}
+	public HashMap<String, String> query() throws IOException {
+	    if (content == null) {
+	        content = fetchContent();
+	    }
 
-		HashMap<String, String> retVal = new HashMap<String, String>();
-		
-		
-		/* 
-		 * some Jsoup source
-		 * https://jsoup.org/apidocs/org/jsoup/nodes/package-summary.html
-		 * https://www.1ju.org/jsoup/jsoup-quick-start
- 		 */
-		
-		//using Jsoup analyze html string
-		Document doc = Jsoup.parse(content);
-		
-		//select particular element(tag) which you want 
-		Elements lis = doc.select("div");
-		lis = lis.select(".kCrYT");
-		
-		for(Element li : lis)
-		{
-			try 
-			{
-				 Element linkElement = li.select("a").first();
-	                if (linkElement == null) {
-	                    continue;
+	    HashMap<String, String> retVal = new HashMap<String, String>();
+
+	    Document doc = Jsoup.parse(content);
+
+	    Elements lis = doc.select("div");
+	    lis = lis.select(".kCrYT");
+
+	    for (Element li : lis) {
+	        try {
+	            Element linkElement = li.select("a").first();
+	            if (linkElement == null) {
+	                continue;
+	            }
+
+	            String linkHref = linkElement.attr("href");
+	            if (linkHref.startsWith("/url?q=")) {
+	                linkHref = linkHref.substring(7);
+	                int end = linkHref.indexOf("&");
+	                if (end != -1) {
+	                    linkHref = linkHref.substring(0, end);
 	                }
 
-	                String linkHref = linkElement.attr("href");
-	                if (linkHref.startsWith("/url?q=")) {
-	                    linkHref = linkHref.substring(7);
-	                    int end = linkHref.indexOf("&");
-	                    if (end != -1) {
-	                        linkHref = linkHref.substring(0, end);
+	                // Decode URL
+	                linkHref = java.net.URLDecoder.decode(linkHref, "UTF-8");
+	                // Check if the decoded string is a valid URL
+	                try {
+	                    URL url = new URL(linkHref);
+	                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+	                    int responseCode = urlConnection.getResponseCode();
+
+	                    if (responseCode == HttpURLConnection.HTTP_OK) {
+	                        retVal.put(linkElement.text(), linkHref);
+	                    } else if (responseCode == HttpURLConnection.HTTP_FORBIDDEN) {
+	                        // HTTP 403 Forbidden
+	                        System.out.println("HTTP 403 Forbidden for URL: " + linkHref);
+	                    } else if (responseCode == HttpURLConnection.HTTP_BAD_REQUEST) {
+	                        // HTTP 400 Bad Request
+	                        System.out.println("HTTP 400 Bad Request for URL: " + linkHref);
+	                    } else {
+	                        // 其他錯誤處理
+	                        System.out.println("Error: " + responseCode + " for URL: " + linkHref);
 	                    }
-
-	                    // Decode URL
-	                    linkHref = java.net.URLDecoder.decode(linkHref, "UTF-8");
-	                    // Check if the decoded string is a valid URL
-	                    try {
-	                        new URL(linkHref);
-	                    } catch (MalformedURLException e) {
-	                        continue; // Skip this result if the URL is not valid
-	                    }
-	                } else {
-	                    continue; // Skip if not a valid link format
+	                } catch (MalformedURLException e) {
+	                    // Handle malformed URL
 	                }
+	            } else {
+	                continue; // Skip if not a valid link format
+	            }
 
-	                String title = linkElement.select(".vvjwJb").text();
-	                if (title.isEmpty()) {
-	                    continue;
-	                }
-
-	                retVal.put(title, linkHref);
-
-
-			} catch (IndexOutOfBoundsException e) 
-			{
-//				e.printStackTrace();
-			}
-		}
-		return retVal;
+	        } catch (Exception e) {
+	            // Handle other exceptions
+	            e.printStackTrace();
+	        }
+	    }
+	    return retVal;
 	}
+
+
+
 }
+
 
